466e4465c39d76d6713f4c44cbef27a64e56d2ad
GonzaL <gonzalopez1004@gmail.com>
2023-07-11 21:05:51 -0300

    URL del index creada

--- cart/urls.py
+++ cart/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cartApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.productsIndex, name="Products"),
 ]
