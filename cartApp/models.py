7ddbc8f1bd9aee9a99da4286f41e6bfbd4d083df
GonzaL <gonzalopez1004@gmail.com>
2023-07-12 19:12:49 -0300

    Product model created

--- cartApp/models.py
+++ cartApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    NEW = 'N'
+    USED = 'U'
+    STATE_CHOICES = (
+        (NEW, 'New'),
+        (USED, 'Used'),
+    )
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    state = models.CharField(max_length=1, choices=STATE_CHOICES, default=NEW)
+
+    def __str__(self):
+        return self.name
