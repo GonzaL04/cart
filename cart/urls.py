c242fff562906ce5e078ac2f98d19b72f35354ba
GonzaL <gonzalopez1004@gmail.com>
2023-07-12 21:17:37 -0300

    URLS configured

--- cart/urls.py
+++ cart/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from cartApp import views
+from django.urls import include, path
+from cart import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.productsIndex, name="Products"),
-]
+    path('', include('cartApp.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
