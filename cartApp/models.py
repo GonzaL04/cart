4d5c71193e6ca1d5da2f9bf03680dda702850346
GonzaL <gonzalopez1004@gmail.com>
2023-07-12 21:18:23 -0300

    Product model created

--- cartApp/models.py
+++ cartApp/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     description = models.TextField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     state = models.CharField(max_length=1, choices=STATE_CHOICES, default=NEW)
+    image = models.ImageField(null=False, blank=False)
 
     def __str__(self):
         return self.name
