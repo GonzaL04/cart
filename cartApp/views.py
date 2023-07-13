b08fa9a917959609d81d4592a1342d6e27563620
GonzaL <gonzalopez1004@gmail.com>
2023-07-12 21:19:39 -0300

    Functions created

--- cartApp/views.py
+++ cartApp/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from .models import Product
 
 # Create your views here.
 
 def productsIndex(request):
-    return render (request, 'index.html')
+    products = Product.objects.all()
+    context = {
+        "products": products
+    }
+    return render (request, 'index.html', context)
+
+def addProduct(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('Products')
+    else:
+        form = ProductForm()
+    
+    return render(request, 'index.html', {'form': form})
+
+def deleteProduct(request, pk):
+    product = Product.objects.get(pk=pk)
+    product.delete()
+    return redirect('Products')
