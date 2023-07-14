69b9cb0384a7561e7cab5be31578ede688f2782c
GonzaL <gonzalopez1004@gmail.com>
2023-07-14 13:53:38 -0300

    Add edit & delete

--- cartApp/urls.py
+++ cartApp/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.productsIndex, name="Products"),
     path('addProduct/', views.addProduct, name='addProduct'),
+    path('edit/<int:pk>/', views.editProduct, name='editProduct'),
     path('deleteProduct/<int:pk>/', views.deleteProduct, name='deleteProduct'),
-]
+    path('cart/remove/<int:product_id>/', views.removeFromCart, name='removeFromCart'),
+    path('cart/', views.cartIndex, name="cartIndex"),
+    path('cart/add/<int:product_id>/', views.addToCart, name='addToCart'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
